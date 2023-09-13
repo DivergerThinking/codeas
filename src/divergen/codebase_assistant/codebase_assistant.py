a5479d12b01e7502bbf691270aba9a65427fa895
mrenner-ML <manuelrenner@Manuels-MacBook-Pro.local>
2023-09-13 11:17:40 +0200

    feat: added parsing to the model output

--- src/divergen/codebase_assistant/codebase_assistant.py
+++ src/divergen/codebase_assistant/codebase_assistant.py
@@ -14,13 +14,16 @@ class CodebaseAssistant(BaseModel, arbitrary_types_allowed=True):
                     "generate_class_docstring", {"code": entity.source_code}
                 )
                 model_output = self.prompt_manager.execute_prompt(prompt)
-                # TODO parse model_output to remove the ```python and ``` from the beginning and end
+                model_output = self.parse_model_output(model_output)
                 new_module_source_code = self.code_manager.modify_source_code(
                     module.source_code, entity.source_code, model_output.content
                 )
                 preview_path = module.path.replace(".py", "_preview.py")
                 self.write_new_source_code(new_module_source_code, preview_path)
                 break # TODO: Remove this break. This is only for testing purposes.
+    
+    def parse_model_output(self, model_output: str):
+        return model_output.replace("```python", "").replace("```", "")
     
     def write_new_source_code(self, code: str, output_path: str):
         with open(output_path, "w") as f:
