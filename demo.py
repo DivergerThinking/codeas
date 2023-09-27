2b61ddb4e6ddde321d584ea31757a1ff704a5aec
mrenner-ML <manuelrenner@yahoo.fr>
2023-09-27 17:11:14 +0200

    refactor: polish demo UI

--- demo.py
+++ demo.py
@@ -16,12 +16,12 @@ class UI(BaseModel):
     _action: str = PrivateAttr(None)
     _template: str = PrivateAttr(None)
     _prompt: str = PrivateAttr(None)
-    
+
     def add_title(self):
         st.title("Divergen")
 
     def show_context_banner(self):
-        st.text("CONTEXT")
+        st.subheader("CONTEXT")
         col1, col2, col3 = st.columns(3)
         with col1:
             self.select_source_dir()
@@ -33,7 +33,7 @@ class UI(BaseModel):
             self.select_entities()
 
     def show_prompt_banner(self):
-        st.text("PROMPT")
+        st.subheader("PROMPT")
         col1, col2, col3 = st.columns([0.45, 0.1, 0.45])
         with col1:
             self.select_template()
@@ -47,7 +47,7 @@ class UI(BaseModel):
             self.write_prompt()
 
     def select_source_dir(self):
-        self._source_dir = st.text_input("Source directory", "src-test")
+        self._source_dir = st.text_input("Source directory")
         if self._source_dir:
             st.text(f"{os.path.abspath(self._source_dir)}")
 
@@ -59,7 +59,9 @@ class UI(BaseModel):
 
     def select_action(self):
         self._action = st.selectbox(
-            "Choose an action to perform", ["Modify codebase", "Generate docs"], index=None
+            "Choose an action to perform",
+            ["Modify codebase", "Generate markdown"],
+            index=None,
         )
 
     def select_entities(self):
@@ -82,6 +84,17 @@ class UI(BaseModel):
         self._prompt = st.text_input("Enter prompt")
         if self._prompt:
             st.session_state["has_prompt"] = True
+
+    def print_prompts(self):
+        for entity_name in self._entities:
+            st.text(
+                st.session_state["assistant"].prompt_manager.build(
+                    template=self._template,
+                    code=st.session_state["assistant"]
+                    .codebase.get_entity(entity_name)
+                    .get_code(),
+                )
+            )
 
     def run_llm(self):
         if self._prompt:
@@ -116,20 +129,22 @@ class UI(BaseModel):
                 preview_code = self._read_file(preview_path)
                 st.text(preview_path)
                 st.code(preview_code)
-    
+
     def show_original_code(self):
-        for module in st.session_state["assistant"].codebase.get_modified_modules():
-            original_path = os.path.join(
-                st.session_state["assistant"].codebase.source_dir, module.path
-            )
-            original_code = self._read_file(original_path)
-            st.text(original_path)
-            st.code(original_code)
+        _, col1, _ = st.columns([2, 4, 2])
+        with col1:
+            for module in st.session_state["assistant"].codebase.get_modified_modules():
+                original_path = os.path.join(
+                    st.session_state["assistant"].codebase.source_dir, module.path
+                )
+                original_code = self._read_file(original_path)
+                st.text(original_path)
+                st.code(original_code)
 
     def _read_file(self, path):
         with open(path, "r") as file_:
             return file_.read()
-    
+
     def show_apply_reject(self):
         apply = st.selectbox(
             "Select change to apply",
@@ -138,17 +153,13 @@ class UI(BaseModel):
         )
         if apply == "Apply changes":
             st.session_state["assistant"].apply_changes()
-            # st.session_state["has_run"] = False
-            # st.session_state["show_code"] = False
             st.text("Changes applied")
             self.show_original_code()
         elif apply == "Reject changes":
             st.session_state["assistant"].reject_changes()
-            # st.session_state["has_run"] = False
-            # st.session_state["show_code"] = False
             st.text("Changes rejected")
             self.show_original_code()
-            
+
 
 def configure_app():
     st.set_page_config(layout="wide")
@@ -164,12 +175,24 @@ def configure_app():
     if "assistant" not in st.session_state:
         st.session_state["assistant"] = None
 
+
 def instantiate_assistant(ui):
     if st.session_state["assistant"] is None and ui._source_dir is not None:
         st.session_state["assistant"] = CodebaseAssistant(
             codebase={"source_dir": ui._source_dir},
             prompt_manager={"prompt_library": "./assets/prompt-library"},
         )
+    elif (
+        st.session_state["assistant"] is not None
+        and ui._source_dir is not None
+        and ui._source_dir != st.session_state["assistant"].codebase.source_dir
+    ):
+        st.session_state["assistant"] = CodebaseAssistant(
+            codebase={"source_dir": ui._source_dir},
+            prompt_manager={"prompt_library": "./assets/prompt-library"},
+        )
+        st.rerun()
+
 
 def main():
     configure_app()
@@ -183,12 +206,14 @@ def main():
         if (
             st.session_state["has_template"] or st.session_state["has_prompt"]
         ) and st.session_state["has_run"] is False:
+            ui.print_prompts()
             ui.run_llm()
             st.session_state["has_run"] = True
             st.session_state["show_code"] = True
         if st.session_state["show_code"]:
             ui.show_code_comparison()
             ui.show_apply_reject()
+
 
 if __name__ == "__main__":
     main()
