ce556867c70d95e20ca80f37ab4dba61d08e0cfb
mrenner-ML <manuelrenner@yahoo.fr>
2023-10-10 12:47:41 +0200

    chore: add examples to test and evaluate tool

--- examples/ml_repo_s/src/ml_pipeline.py
+++ examples/ml_repo_s/src/ml_pipeline.py
@@ -1,7 +1,62 @@
 import pandas as pd
+import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report
+
+
+class ProcessingPipeline:
+    def __init__(self):
+        self.data = None
+
+    def load_data(self, file_path):
+        try:
+            self.data = pd.read_csv(file_path)
+            print("Data loaded successfully.")
+        except Exception as e:
+            print(f"Error loading data: {str(e)}")
+
+    def explore_data(self):
+        if self.data is not None:
+            print("Data Exploration:")
+            print(self.data.describe())
+        else:
+            print("No data loaded for exploration.")
+
+    def clean_data(self):
+        if self.data is not None:
+            print("Data Cleaning:")
+            self.data = self.data.drop_duplicates()
+            self.data = self.data.dropna()
+            print("Data cleaned.")
+        else:
+            print("No data loaded for cleaning.")
+
+    def filter_data(self, condition):
+        if self.data is not None:
+            print("Data Filtering:")
+            self.data = self.data[condition]
+            print("Data filtered.")
+        else:
+            print("No data loaded for filtering.")
+
+    def aggregate_data(self, group_by, aggregations):
+        if self.data is not None:
+            print("Data Aggregation:")
+            self.data = self.data.groupby(group_by).agg(aggregations)
+            print("Data aggregated.")
+        else:
+            print("No data loaded for aggregation.")
+
+    def export_data(self, output_path):
+        if self.data is not None:
+            try:
+                self.data.to_csv(output_path, index=False)
+                print("Data exported successfully.")
+            except Exception as e:
+                print(f"Error exporting data: {str(e)}")
+        else:
+            print("No data available for export.")
 
 
 class Predictor:
@@ -56,8 +111,18 @@ class Predictor:
 
 
 if __name__ == "__main__":
+    pipeline = ProcessingPipeline()
+    pipeline.load_data("sample_data.csv")
+    pipeline.explore_data()
+    pipeline.clean_data()
+    pipeline.filter_data(condition=(pipeline.data["column_name"] > 10))
+    pipeline.aggregate_data(
+        group_by="group_column", aggregations={"value_column": "mean"}
+    )
+    pipeline.export_data("output_data.csv")
+
     predictor = Predictor()
-    predictor.load_data("your_dataset.csv")
+    predictor.load_data("sample_data.csv")
     predictor.feature_engineering(
         target_column="target_column", drop_columns=["column_to_drop"]
     )
