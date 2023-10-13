c02e9e5f352c00abbc7a526474721af33bb2cc42
mrenner-ML <manuelrenner@yahoo.fr>
2023-10-13 11:10:50 +0200

    feat: execute cli with prompt name from prompts.yaml

--- src/divergen/cli.py
+++ src/divergen/cli.py
@@ -4,7 +4,8 @@ from typing import Optional
 from typing_extensions import Annotated
 
 load_dotenv()
-
+from langchain.chat_models.fake import FakeMessagesListChatModel
+from langchain.schema import AIMessage
 from divergen.codebase_assistant import CodebaseAssistant
 from divergen.cli_inputs import (
     input_modules,
@@ -12,12 +13,24 @@ from divergen.cli_inputs import (
     input_apply_changes,
     input_context,
     input_target,
-    input_guidelines,
-    input_preprompt,
+    input_guidelines
 )
 
+dummy_func = """
+def dummy_func():
+    pass
+"""
+msg = AIMessage(content=dummy_func)
+dummy_model = FakeMessagesListChatModel(responses=[msg])
+
 app = typer.Typer()
-assistant = CodebaseAssistant()
+assistant = CodebaseAssistant(
+    codebase={"root": "./ml_repo_xs"},
+    preprompts_path="./configs/preprompts.yaml",
+    guidelines_path="./configs/guidelines.yaml",
+    # model=dummy_model,
+    max_tokens_per_module=2000
+)
 
 
 @app.command()
@@ -37,35 +50,29 @@ def redo():
 
 @app.command()
 def run(
-    prompt: Annotated[
-        Optional[str], typer.Argument(help="Either the prompt OR preprompt name to run")
+    prompt_name: Annotated[
+        Optional[str], typer.Argument(help="The name of the prompt found in prompts.yaml")
     ] = None,
-    use_preprompt: Annotated[
+    use_inputs: Annotated[
         Optional[bool],
-        typer.Option("-p", "--preprompt", help="whether to use preprompt"),
+        typer.Option("-i", "--inputs", help="whether to use CLI inputs to run prompt"),
     ] = False,
     use_default: Annotated[Optional[bool], typer.Option("-d")] = False,
 ):
-    if use_preprompt:
-        if prompt is None:
-            prompt = input_preprompt(assistant)
-        modules = input_modules(assistant, use_default)
-        # assistant.execute_preprompt(prompt, modules)
-        print(prompt, modules)
-    else:
-        if prompt is None:
-            prompt = input_prompt()
+    if use_inputs:
+        prompt = input_prompt()
         modules = input_modules(assistant, use_default)
         context = input_context(use_default)
         target = input_target(use_default)
         guidelines = input_guidelines(assistant, use_default)
-        print(prompt, context, target, guidelines, modules)
-        # assistant.execute_prompt(prompt, context, target, guidelines, modules)
-
+        assistant.execute_prompt(prompt, context, target, guidelines, modules)
+    else:
+        modules = input_modules(assistant, use_default)
+        assistant.execute_preprompt(prompt_name, modules)
+    
     apply = input_apply_changes()
     if apply:
-        print("apply changes")
-        # assistant.apply_changes()
+        assistant.apply_changes()
 
 
 if __name__ == "__main__":
