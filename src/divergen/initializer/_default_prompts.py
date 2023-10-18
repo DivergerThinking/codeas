a7774c465fc44941daa63eb94c678ffda111f3c3
mrenner-ML <manuelrenner@yahoo.fr>
2023-10-18 11:50:00 +0200

    feat: export yaml in more readable format

--- src/divergen/initializer/_default_prompts.py
+++ src/divergen/initializer/_default_prompts.py
@@ -1,14 +1,13 @@
 DEFAULT_PROMPTS = {
     "generate_docstrings": {
-        "user_prompt": "Generate docstrings using numpy style.",
         "action": "modify_code",
+        "user_prompt": "Generate docstrings using numpy style.",
         "guidelines": ["clean_code"],
     },
     "guidelines": {
-        "clean_code": """
-Use consistent and meaningful naming conventions for variables, functions, classes, and modules.
+        "clean_code": """Use consistent and meaningful naming conventions for variables, functions, classes, and modules.
 Don't repeat yourself. Reuse code through functions, classes, or libraries to minimize duplication.
 Add comments to explain complex logic, but aim to write self-explanatory code.
 """
-    }
+    },
 }
--- src/divergen/utils.py
+++ src/divergen/utils.py
@@ -22,8 +22,37 @@ def read_yaml(path):
 
 
 def write_yaml(path, data):
+    yaml.add_representer(str, str_presenter)
+    yaml.representer.SafeRepresenter.add_representer(str, str_presenter)
     with open(path, "w") as yaml_file:
-        yaml.dump(data, yaml_file, default_flow_style=False)
+        yaml.dump(
+            data,
+            yaml_file,
+            Dumper=SpaciousDumper,
+            default_flow_style=False,
+            sort_keys=False,
+        )
+
+
+class SpaciousDumper(yaml.SafeDumper):
+    """
+    HACK: insert blank lines between top-level objects (used in write_yaml)
+    inspired by https://stackoverflow.com/a/44284819/3786245
+    """
+
+    def write_line_break(self, data=None):
+        super().write_line_break(data)
+        if len(self.indents) == 1:
+            super().write_line_break()
+
+
+def str_presenter(dumper, data):
+    """configures yaml for dumping multiline strings
+    Ref: https://stackoverflow.com/questions/8640959/how-can-i-control-what-scalar-form-pyyaml-uses-for-my-data
+    """
+    if data.count("\n") > 0:  # check for multiline string
+        return dumper.represent_scalar("tag:yaml.org,2002:str", data, style="|")
+    return dumper.represent_scalar("tag:yaml.org,2002:str", data)
 
 
 def copy_files(source_path, target_path):
