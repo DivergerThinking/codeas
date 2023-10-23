1a52bf452b07f9cf1517c7569cd2b5562915df67
mrenner-ML <manuelrenner@yahoo.fr>
2023-10-23 11:55:53 +0200

    feat: add comments in default prompts.yaml

--- src/divergen/initializer.py
+++ src/divergen/initializer.py
@@ -3,9 +3,9 @@ from __future__ import annotations
 import os
 from typing import TYPE_CHECKING
 
+import yaml
 from pydantic import BaseModel
 
-from divergen._prompts import DEFAULT_PROMPTS
 from divergen.utils import copy_files, write_yaml
 
 if TYPE_CHECKING:
@@ -19,8 +19,26 @@ class Initializer(BaseModel):
             copy_files(source_path, ".divergen")
         else:
             write_yaml(".divergen/assistant.yaml", assistant.model_dump())
-            write_yaml(".divergen/prompts.yaml", DEFAULT_PROMPTS)
+            self._write_default_prompts(".divergen/prompts.yaml")
 
     def _create_divergen_dir(self):
         if not os.path.exists(".divergen"):
             os.mkdir(".divergen")
+
+    def _write_default_prompts(self, file_path: str):
+        with open(file_path, "w") as yaml_file:
+            yaml.dump(
+                {"PROMPT_NAME": {"instructions": "YOUR INSTRUCTIONS HERE"}},
+                yaml_file,
+                default_flow_style=False,
+                sort_keys=False,
+            )
+            yaml_file.write(
+                "  # target: code\n  # context: code\n  # guidelines:\n  #   - GUIDELINE_NAME\n\n"
+            )
+            yaml.dump(
+                {"guidelines": {"GUIDELINE_NAME": "YOUR GUIDELINE HERE"}},
+                yaml_file,
+                default_flow_style=False,
+                sort_keys=False,
+            )
