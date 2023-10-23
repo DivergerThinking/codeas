4be57b76121ca8240c21ce5d9f82de58ec12588b
mrenner-ML <manuelrenner@yahoo.fr>
2023-10-23 10:02:23 +0200

    chore: define prompts.yaml example

--- src/divergen/_prompts.py
+++ src/divergen/_prompts.py
@@ -1,13 +1,4 @@
 DEFAULT_PROMPTS = {
-    "generate_docstrings": {
-        "instructions": "Generate docstrings using numpy style.",
-        "target": "code",
-        "guidelines": ["clean_code"],
-    },
-    "guidelines": {
-        "clean_code": """Use consistent and meaningful naming conventions for variables, functions, classes, and modules.
-Don't repeat yourself. Reuse code through functions, classes, or libraries to minimize duplication.
-Add comments to explain complex logic, but aim to write self-explanatory code.
-"""
-    },
+    "PROMPT_NAME": {"instructions": "YOUR INSTRUCTIONS HERE"},
+    "guidelines": {"GUIDELINE_NAME": "YOUR GUIDELINE HERE"},
 }
