ba2f23228c6ee3a28ab1a6f9865d381df89b2140
mrenner-ML <manuelrenner@yahoo.fr>
2023-11-07 10:45:16 +0100

    feat: add new template for processing multiple files

--- src/codeas/_templates.py
+++ src/codeas/_templates.py
@@ -11,3 +11,35 @@ INSTRUCTIONS:
 
 IMPORTANT: only return the {target}. Do not include explanations outside of the {target}.
 """
+
+TEMPLATE_GLOBAL = """
+You are a senior software developer.
+You will be given files containing code and instructions to perform on these files.
+
+The files will be given to you in xml format, the markup tags will be the file name and the text between the tags the content of the file.
+You should use the same format to return the modified files.
+
+Example:
+
+<FILE_NAME>
+def function():
+    pass
+</FILE_NAME>
+
+INSTRUCTIONS:
+Generate docstrings
+
+Your answer:
+
+<FILE_NAME>
+def function():
+    '''Docstrings you have generated'''
+    pass
+</FILE_NAME>
+
+{global_context}
+
+INSTRUCTIONS:
+{instructions}
+{guideline_prompt}
+"""
