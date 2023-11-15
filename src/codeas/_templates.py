d5d31c8786ee5434cf351df8b44a9c333c41b832
mrenner-ML <manuelrenner@yahoo.fr>
2023-11-15 11:48:54 +0100

    feat: add prompts to handle dynamic file creation

--- src/codeas/_templates.py
+++ src/codeas/_templates.py
@@ -37,6 +37,35 @@ def function():
     pass
 </FILE_NAME>
 
+In the case you are asked to generate new files in the request, this should be reflected in the file name. Use dots instead of / as path separator.
+Example request:
+
+<FILE_NAME1>
+def function1():
+    pass
+</FILE_NAME1>
+
+<FILE_NAME2>
+def function2():
+    pass
+</FILE_NAME2>
+
+INSTRUCTIONS:
+Generate tests for the files. Write the tests in separate files under a tests/ folder. 
+The file names should start with "test_" as prefix followed by the original file name.
+
+Example answer:
+
+<tests.test_FILE_NAME1>
+def test_function1():
+    pass
+</tests.test_FILE_NAME1>
+
+<tests.test_FILE_NAME2>
+def test_function2():
+    pass
+</tests.test_FILE_NAME2>
+
 Request:
 {global_context}
 
@@ -51,6 +80,7 @@ TEMPLATE_MODULES = """
 You are a senior software developer.
 You will be given the structure of the codebase and some instructions which should be performed on that codebase.
 You should return the full path of the modules which are relevant to these instructions in csv format.
+IMPORTANT: only return paths which are already present in the repository structure. Don't include any new paths, even if they appear in the instructions.
 
 Example request:
 
@@ -77,3 +107,50 @@ INSTRUCTIONS:
 
 Answer:
 """
+
+SYSTEM_PROMPT_GLOBAL = '''
+You are an intelligent system who helps modify a codebase based on user requests.
+
+You will be given parts of the codebase in an XML format, with the file paths appearing as XML tags and the content of the files appearing between the tags. IMPORTANT NOTE: the path separator used is "." instead of "/"
+
+Your output should follow the same format as it will be parsed in order to modify and create files. 
+
+Be careful about the path of the file you output. 
+When the request is related to modifying existing files, you should return the exact same file path. Example:
+"""
+<path.file_name>
+code
+</path.file_name>
+
+Add docstrings using numpy style.
+
+<path.file_name>
+code with docstrings
+</path.file_name>
+"""
+
+When the request is related to creating new files in a different path or with a different name, you should return a different file path. Example:
+"""
+<path.file_name>
+code
+</path.file_name>
+
+Generate tests using pytest. Create separate files in the tests/ folder, with a prefix "test_" followed by the file name.
+
+<tests.test_file_name>
+tests
+</tests.test_file_name>
+"""
+Make sure you consider the file extension in your output as well. If you are asked to generate an output in a different format than the original file, this should be reflected in your output. Example:
+"""
+<path.file_name.py>
+code
+</path.file_name.py>
+
+Generate documentation in markdown format. Create separate files in the docs/ folder.
+
+<docs.file_name.md>
+tests
+</docs.file_name.md>
+"""
+'''
