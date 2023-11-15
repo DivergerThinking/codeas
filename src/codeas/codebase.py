8792d3966e4bd2ec5468190f9970394f2973b1bd
mrenner-ML <manuelrenner@yahoo.fr>
2023-11-15 11:48:32 +0100

    feat: move utils.tree() to codebase.get_tree() and fix codebase.parse_modules() and get_modules()

--- src/codeas/codebase.py
+++ src/codeas/codebase.py
@@ -1,5 +1,6 @@
-import glob
 import os
+from fnmatch import fnmatch
+from pathlib import Path
 from typing import List
 
 import tree_sitter_languages
@@ -20,36 +21,25 @@ LANG_EXTENSION_MAP = {
     ".go": "go",
     ".php": "php",
 }
+DEFAULT_FILE_PATTERNS = [f"*{ext}" for ext in LANG_EXTENSION_MAP.keys()]
+DEFAULT_EXCLUDE_PATTERNS = [".*", "__*"]
 
 
 class Codebase(BaseModel):
     """Codebase is a collection of modules, while a module is a collection of entities,
     such as functions and classes, which are parsed from source code files.
     See the Module class for more information.
-
-    Attributes
-    ----------
-    code_folder : str, optional
-        folder where the source code is found, by default "./src/"
-    docs_folder : str, optional
-        folder where the documentation is found, by default "./docs/"
-    tests_folder : str, optional
-        folder where the tests are found, by default "./tests/"
-    docs_format : str, optional
-        the documnetation format, by default ".md"
     """
 
-    code_folder: str = "./src/"
-    docs_folder: str = "./docs/"
-    tests_folder: str = "./tests/"
-    docs_format: str = ".md"
+    exclude_patterns: list = DEFAULT_EXCLUDE_PATTERNS
+    include_file_patterns: list = DEFAULT_FILE_PATTERNS
     _modules: List[Module] = PrivateAttr(default_factory=list)
     _parser: Parser = PrivateAttr(None)
 
     def parse_modules(self):
         """Parse all the modules in the code folder and save them in the modules list."""
         self._check_code_folder()
-        for module_path in self.get_modules_paths(self.code_folder):
+        for module_path in self.get_modules_paths():
             self.parse_module(module_path)
 
     def _check_code_folder(self):
@@ -58,19 +48,71 @@ class Codebase(BaseModel):
                 f"Source code folder {self.code_folder} not found. Check your configurations in the assistant.yaml file."
             )
 
-    def get_modules_paths(self, path):
-        module_paths = []
-        for ext in LANG_EXTENSION_MAP.keys():
-            module_paths.extend(
-                [
-                    file_path
-                    for file_path in glob.glob(f"{path}/**/*{ext}", recursive=True)
-                ]
-            )
-        return self._ignore_init_files(module_paths)
+    def get_tree(self):
+        tree = ""
+        for path_element in self._get_tree_recursively("."):
+            tree += f"{path_element}\n"
+        return tree
 
-    def _ignore_init_files(self, files):
-        return [file_ for file_ in files if os.path.split(file_)[-1] != "__init__.py"]
+    def _get_tree_recursively(self, path: str, prefix: str = ""):
+        paths = self._get_matching_paths(path)
+        space = "    "
+        branch = "│   "
+        tee = "├── "
+        last = "└── "
+        # paths each get pointers that are ├── with a final └── :
+        pointers = [tee] * (len(paths) - 1) + [last]
+        for pointer, path in zip(pointers, paths):
+            if path.is_dir() and self._match(path, self.include_file_patterns, True):
+                yield prefix + pointer + path.name + "/"
+            elif path.is_file():
+                yield prefix + pointer + path.name
+
+            if path.is_dir():  # extend the prefix and recurse:
+                extension = branch if pointer == tee else space
+                # i.e. space because last, └── , above so no more |
+                yield from self._get_tree_recursively(path, prefix=prefix + extension)
+
+    def _get_matching_paths(self, path):
+        return list(
+            path
+            for path in Path(path).iterdir()
+            if self._not_match(path, self.exclude_patterns)
+            and self._match(path, self.include_file_patterns)
+        )
+
+    def get_modules_paths(self):
+        paths = []
+        for path in self._get_paths_recursively("."):
+            paths.append(path)
+        return paths
+
+    def _get_paths_recursively(self, path: str):
+        paths = self._get_matching_paths(path)
+        for path in paths:
+            if path.is_dir():
+                yield from self._get_paths_recursively(path)
+            else:
+                yield str(path)
+
+    def _not_match(self, path: Path, patterns: list):
+        if any(patterns):
+            for pattern in patterns:
+                if fnmatch(path.name, pattern):
+                    return False
+            return True
+        else:
+            return True
+
+    def _match(self, path: Path, file_patterns: list, match_dir: bool = False):
+        if any(file_patterns):
+            if path.is_file():
+                return any([fnmatch(path.name, pattern) for pattern in file_patterns])
+            if match_dir and path.is_dir():
+                return any(
+                    [any(list(path.glob(f"**/{pattern}"))) for pattern in file_patterns]
+                )
+        return True
 
     def parse_module(self, path: str):
         """Parse a module from a source code file.
@@ -103,7 +145,13 @@ class Codebase(BaseModel):
         if module_names is None:
             return self._modules
         else:
-            return [self.get_module(module_name) for module_name in module_names]
+            modules = []
+            for module_name in module_names:
+                try:
+                    modules.append(self.get_module(module_name))
+                except ValueError:
+                    pass
+            return modules
 
     def get_module_names(self):
         """Return a list of module names."""
@@ -114,6 +162,9 @@ class Codebase(BaseModel):
             if module.name == name:
                 return module
         raise ValueError(f"Module {name} not found")
+
+    def add_module(self, name, content):
+        self._modules.append(Module(name=name, new_content=content, modified=True))
 
     def get_modified_modules(self):
         """Return a list of modules that have been modified."""
--- src/codeas/utils.py
+++ src/codeas/utils.py
@@ -1,9 +1,6 @@
-import fnmatch
 import logging
 import os
-from pathlib import Path
 from shutil import copyfile, copytree
-from typing import List
 
 import yaml
 from tiktoken import encoding_for_model
@@ -64,59 +61,3 @@ def copy_files(source_path, target_path):
         copytree(source_path, target_path)
     else:
         copyfile(source_path, target_path)
-
-
-def tree(dir_path: str, exclude_patterns: List[str] = None):
-    """A recursive generator, given a directory Path object
-    will yield a visual tree structure line by line
-    with each line prefixed by the same characters
-    """
-    # prefix components:
-    space = "    "
-    branch = "│   "
-    # pointers:
-    tee = "├── "
-    last = "└── "
-
-    def _tree(
-        dir_path: str,
-        prefix: str = "",
-        exclude_patterns: List[str] = None,
-        include_file_patterns: List[str] = None,
-    ):
-        contents = list(
-            path
-            for path in Path(dir_path).iterdir()
-            if _not_match(path, exclude_patterns)
-            and _match(path, include_file_patterns)
-        )
-        # contents each get pointers that are ├── with a final └── :
-        pointers = [tee] * (len(contents) - 1) + [last]
-        for pointer, path in zip(pointers, contents):
-            yield prefix + pointer + path.name + "/" if path.is_dir() else prefix + pointer + path.name
-            if path.is_dir():  # extend the prefix and recurse:
-                extension = branch if pointer == tee else space
-                # i.e. space because last, └── , above so no more |
-                yield from _tree(
-                    path, prefix=prefix + extension, exclude=exclude_patterns
-                )
-
-    def _not_match(path: Path, patterns: List[str] = None):
-        if any(patterns):
-            for pattern in patterns:
-                if fnmatch.fnmatch(path.name, pattern):
-                    return False
-            return True
-        else:
-            return True
-
-    def _match(path: Path, file_patterns: list):
-        if path.is_file() and any(file_patterns):
-            return any([fnmatch(path.name, pattern) for pattern in file_patterns])
-        else:
-            return True
-
-    tree_str = ""
-    for line in _tree(dir_path, exclude=exclude_patterns):
-        tree_str += f"{line}\n"
-    return tree_str
