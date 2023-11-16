624db1bc0adc4faf4f83c49db14fa3805dc24ea3
mrenner-ML <manuelrenner@yahoo.fr>
2023-11-16 16:12:25 +0100

    feat: make system prompts clearer

--- src/codeas/_templates.py
+++ src/codeas/_templates.py
@@ -1,158 +1,48 @@
-TEMPLATE = """
-You are a senior software developer.
-You will be given some {context} and instructions to perform on that {context}. 
-
-{CONTEXT}:
-{entity_context}
-
-INSTRUCTIONS:
-{instructions}
-{guideline_prompt}
-
-IMPORTANT: only return the {target}. Do not include explanations outside of the {target}.
-"""
-
-TEMPLATE_GLOBAL = """
-You are a senior software developer.
-You will be given files containing code and instructions to perform on these files.
-
-The files will be given to you in xml format, the markup tags will be the file name and the text between the tags the content of the file.
-You should use the same format to return the modified files.
-
-Example request:
-
-<FILE_NAME>
-def function():
-    pass
-</FILE_NAME>
-
-INSTRUCTIONS:
-Generate docstrings
-
-Example answer:
-
-<FILE_NAME>
-def function():
-    '''Docstrings you have generated'''
-    pass
-</FILE_NAME>
-
-In the case you are asked to generate new files in the request, this should be reflected in the file name. Use dots instead of / as path separator.
-Example request:
-
-<FILE_NAME1>
-def function1():
-    pass
-</FILE_NAME1>
-
-<FILE_NAME2>
-def function2():
-    pass
-</FILE_NAME2>
-
-INSTRUCTIONS:
-Generate tests for the files. Write the tests in separate files under a tests/ folder. 
-The file names should start with "test_" as prefix followed by the original file name.
-
-Example answer:
-
-<tests.test_FILE_NAME1>
-def test_function1():
-    pass
-</tests.test_FILE_NAME1>
-
-<tests.test_FILE_NAME2>
-def test_function2():
-    pass
-</tests.test_FILE_NAME2>
-
-Request:
-{global_context}
-
-INSTRUCTIONS:
-{instructions}
-{guideline_prompt}
-
-Answer:
-"""
-
-TEMPLATE_MODULES = """
-You are a senior software developer.
-You will be given the structure of the codebase and some instructions which should be performed on that codebase.
-You should return the full path of the modules which are relevant to these instructions in csv format.
-IMPORTANT: only return paths which are already present in the repository structure. Don't include any new paths, even if they appear in the instructions.
-
-Example request:
-
-src/
-├── codeas/
-│   ├── _templates.py
-│   ├── assistant.py
-│   ├── cli.py
-│   ├── cli_inputs.py
-
-INSTRUCTIONS:
-Move the overwrite_configs method in assistant.py to cli.py.
-
-Example answer:
-src/codeas/assistant.py,src/codeas/cli.py
-
-Request:
-
-{dir_structure}
-
-INSTRUCTIONS:
-{instructions}
-{guideline_prompt}
-
-Answer:
-"""
-
 SYSTEM_PROMPT_GLOBAL = '''
-You are an intelligent system who helps modify a codebase based on user requests.
+You are an intelligent file system capable of modifying and creating files based on user requests.
 
-You will be given parts of the codebase in an XML format, with the file paths appearing as XML tags and the content of the files appearing between the tags. IMPORTANT NOTE: the path separator used is "." instead of "/"
+You will be given the content of files belonging to a codebase in XML format, and some instructions that need to be carried on that codebase, such as creating tests, writing documentation, or modifying code.
 
-Your output should follow the same format as it will be parsed in order to modify and create files. 
+The file paths will be the XML tags and the content of the files appear between the tags. IMPORTANT: the path separator used is "." instead of "/"
 
-Be careful about the path of the file you output. 
-When the request is related to modifying existing files, you should return the exact same file path. Example:
+You will be given the file contents, the instructions to perform on them as well as the output format you should follow.
+
+The output format contains placeholders as [FILE_CONTENT], which you should fill in based on the instructions. Make sure you use the relevant file as context. 
+Example:
 """
-<path.file_name>
-code
-</path.file_name>
+<src.hello_world.py>
+def hello_world():
+    print("hello world")
+</src.hello_world.py>
+<src.return_hello.py>
+def return_hello():
+    return "hello"
+</src.return_hello.py>
 
-Add docstrings using numpy style.
+Instructions:
+Add docstrings to hello_world.py and return_hello.py
 
-<path.file_name>
-code with docstrings
-</path.file_name>
+Output format:
+<src.hello_world.py>
+[FILE_CONTENT]
+</src.hello_world.py>
+<src.return_hello.py>
+[FILE_CONTENT]
+</src.return_hello.py>
+
+<src.hello_world.py>
+def hello_world():
+    """Prints hello world"""
+    print("hello world")
+</src.hello_world.py>
+<src.return_hello.py>
+def return_hello():
+    """Return hello"""
+    return hello
+</src.return_hello.py>
 """
 
-When the request is related to creating new files in a different path or with a different name, you should return a different file path. Example:
-"""
-<path.file_name>
-code
-</path.file_name>
-
-Generate tests using pytest. Create separate files in the tests/ folder, with a prefix "test_" followed by the file name.
-
-<tests.test_file_name>
-tests
-</tests.test_file_name>
-"""
-Make sure you consider the file extension in your output as well. If you are asked to generate an output in a different format than the original file, this should be reflected in your output. Example:
-"""
-<path.file_name.py>
-code
-</path.file_name.py>
-
-Generate documentation in markdown format. Create separate files in the docs/ folder.
-
-<docs.file_name.md>
-tests
-</docs.file_name.md>
-"""
+The same applies when asked to create new files based on the files given in context.
 '''
 
 SYSTEM_PROMPT_MODULES = '''
@@ -165,7 +55,7 @@ Some of these instructions require files to be read and modified (ex. modifying 
 If no file name is specified in the instructions, you should return all of the files you think are relevant.
 
 You should return the files to read and modify or create in XML format. The tag names should be <read> and <modify> or <create> and the file paths should appear between the corresponding tags in CSV format.
-IMPORTANT: the file paths should use "." instead of "/" as path separators.
+IMPORTANT: the path separator used is "." instead of "/"
 
 Example when the request is related to modifying existing files:
 """
@@ -200,9 +90,9 @@ Generate tests for file_handler.py and assistant.py using pytest. Write the file
 src.codeas.file_handler.py,src.codeas.assistant.py
 </read>
 
-<write>
+<create>
 tests.codeas.test_file_handler.py,tests.codeas.test_assistant.py
-</write>
+</create>
 """
 
 BE CAREFUL WITH FILE FORMAT, some requests might require you to create files with a different format than the original file. Example:
@@ -219,8 +109,8 @@ Generate documentation for request.py in markdown format. Write the documentatio
 src.codeas.request.py
 </read>
 
-<write>
+<create>
 docs.codeas.request.md
-</write>
+</create>
 """
 '''
