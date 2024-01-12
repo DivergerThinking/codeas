fdb229dc05a3b9d8fa269943d8277016640db36f
mrenner-ML <manuelrenner@yahoo.fr>
2024-01-12 18:54:49 +0100

    feat: added terminal autocomplete

--- src/codeas/terminal.py
+++ src/codeas/terminal.py
@@ -1,8 +1,41 @@
 from prompt_toolkit import PromptSession
+from prompt_toolkit.completion import Completer, Completion
 
 from codeas import configs
 from codeas.chat import Chat
+from codeas.codebase import Codebase
 from codeas.utils import end_message_block, start_message_block, write_yaml
+
+
+class AutoCompleter(Completer):
+    commands = ["view", "copy", "clear"]
+    agents = ["search", "context", "write"]
+    relative_files = Codebase().get_modules_paths()
+
+    def get_completions(self, document, _):
+        text = document.text_before_cursor
+        words = text.split()
+        last_word = words[-1]
+        if text[0] == "/" and len(words) == 1 and text[-1] != " ":
+            for completion in self.get_commands(text):
+                yield Completion(completion, -len(text) + 1)
+        elif last_word.startswith("@") and text[-1] != " ":
+            for completion in self.get_agents(last_word):
+                yield Completion(completion, -len(last_word) + 1)
+        elif "/" in last_word and text[-1] != " ":
+            for completion in self.get_relative_files(last_word):
+                yield Completion(completion, -len(last_word))
+
+    def get_commands(self, text):
+        return [
+            command for command in self.commands if ("/" + command).startswith(text)
+        ]
+
+    def get_agents(self, last_word):
+        return [agent for agent in self.agents if ("@" + agent).startswith(last_word)]
+
+    def get_relative_files(self, last_word):
+        return [file_ for file_ in self.relative_files if file_.startswith(last_word)]
 
 
 def start_terminal():
@@ -10,7 +43,7 @@ def start_terminal():
     while True:
         try:
             start_message_block("User input", "bold magenta")
-            session = PromptSession(message="> ")
+            session = PromptSession(message="> ", completer=AutoCompleter())
             message = session.prompt()
         except KeyboardInterrupt:
             break
